888d245aa6b2d98fe3df4e5f18a8bc0f3a7f788d
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2023-10-05 19:48:51 +0530

    Add files via upload

---
+++ (Sorted) Find_Pair.cpp
@@ -0,0 +1,32 @@
+// find pair a + b == k
+// for sorted array
+// don't give duplicate numbers as input
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+void find_pair(int *Arr, int n, int k)
+{
+	int i = 0, j = n - 1;
+
+	while (i != j)
+	{
+		if (k < Arr[i] + Arr[j])
+			j--;
+		else if (k > Arr[i] + Arr[j])
+			i++;
+		else
+			printf("%d %d\n", Arr[i++], Arr[j--]);
+	}
+}
+
+int main(void)
+{
+
+	int Arr[10] = {1, 3, 4, 5, 6, 8, 9, 10, 12, 14};
+
+	find_pair(Arr, 10, 10);
+
+	return 0;
+}
